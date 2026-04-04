6fd74afa02efa4f99d1e2102f864856b2691ae14
Rustam Abuzyarov <rustamabuzarov56@gmail.com>
2026-04-04 12:11:10 +0500

    add controllers

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home_view(request):
+    return render(request, 'catalog/home.html')
+
+def contacts_view(request):
+    return render(request, 'catalog/contacts.html')
