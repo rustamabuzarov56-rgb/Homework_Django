d82a48e981c78681ba5be62776d5acaadc73571c
Rustam Abuzyarov <rustamabuzarov56@gmail.com>
2026-05-15 13:47:25 +0500

    add blog migrations

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+
+    title = models.CharField(max_length=250, verbose_name="заголовок")
+    content = models.TextField(verbose_name="содержимое")
+    preview = models.ImageField(upload_to="blog/", verbose_name="превью")
+    created_at = models.DateField(auto_now_add=True, verbose_name="дата создания")
+    is_published = models.BinaryField(default=True, verbose_name="признак публикации")
+    views_count = models.IntegerField(default=0, verbose_name="количество просмотров")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+
+        verbose_name = "Блоговая запись"
+        verbose_name_plural = "Блоговые записи"
+        ordering = ["-created_at"]
