6cf44cdffbf5941635634b116cab5f9b2b309e0f
Rustam Abuzyarov <rustamabuzarov56@gmail.com>
2026-05-15 11:38:32 +0500

    add app 'blog'

--- config/urls.py
+++ config/urls.py
@@ -7,7 +7,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('catalog/', include('catalog.urls', namespace='catalog'))
+    path('catalog/', include('catalog.urls', namespace='catalog')),
+    path('blog/', include('blog.urls', namespace='blog')),
 ]
 
 if settings.DEBUG:
