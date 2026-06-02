66dbe7081a23fe1c9e40d1bec47a3d490c5440fb
Rustam Abuzyarov <rustamabuzarov56@gmail.com>
2026-06-02 20:07:09 +0500

    Adding styling to product forms

--- catalog/forms.py
+++ catalog/forms.py
@@ -6,3 +6,56 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = ['name', 'description', 'image', 'category', 'price',]
+
+    FORBIDDEN_WORDS = [
+        'казино', 'криптовалюта', 'крипта', 'биржа',
+        'дешево', 'бесплатно', 'обман', 'полиция', 'радар'
+    ]
+
+    def clean_name(self):
+        name = self.cleaned_data.get('name')
+        word_to_check = name.lower()
+
+        for word in self.FORBIDDEN_WORDS:
+            if word in word_to_check:
+                raise ValidationError(f"В названии товара используется запрещенное слово: {word}")
+        return name
+
+    def clean_description(self):
+        description = self.cleaned_data.get('description')
+        word_to_check = description.lower()
+
+        for word in self.FORBIDDEN_WORDS:
+            if word in word_to_check:
+                raise ValidationError(f"В описании товара используется запрещенное слово: {word}")
+        return description
+
+    def clean_price(self):
+        price = self.cleaned_data.get('price')
+        if price <= 0:
+            raise ValidationError("Цена должна быть больше нуля.")
+        return price
+
+    def __init__(self, *args, **kwargs):
+        super(ProductForm, self).__init__(*args, **kwargs)
+        self.fields['name'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Введите название'
+        })
+
+        self.fields['description'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Описание товара'
+        })
+
+        self.fields['image'].widget.attrs.update({
+            'class': 'form-control',
+        })
+
+        self.fields['category'].widget.attrs.update({
+            'class': 'form-control',
+        })
+
+        self.fields['price'].widget.attrs.update({
+            'class': 'form-control',
+        })
